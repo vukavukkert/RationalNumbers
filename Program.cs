92f05936c2423460135bcde73cda5aa97e4695f9
VukkertAS <VukkertAS@411D-2>
2023-03-25 11:38:58 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using RationalNumbers;
+
+//создать класс RationalNumbers
+RationalNum Num1 = new RationalNum(2, 3);
+RationalNum Num2 = new RationalNum(3, 4);
+RationalNum Num3 = Num1 * Num2;
+RationalNum Num4 = Num1 + Num2;
+RationalNum Num5 = Num1 - Num2;
+RationalNum Num6 = Num1 / Num2;
+Console.WriteLine(Num1.PrintRN());
+Console.WriteLine(Num2.PrintRN());
+Console.WriteLine(Num3.PrintRN());
+Console.WriteLine(Num4.PrintRN());
+Console.WriteLine(Num5.PrintRN());
+Console.WriteLine(Num6.PrintRN());
+Console.WriteLine(Num1 > Num2);
---
+++ RationalNum.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RationalNumbers
+{
+    class RationalNum
+    {
+        private int M { get; set; }
+        private int N { get; set; }
+
+        public RationalNum(int m, int n)
+        {
+            M = m;
+            N = n;
+        }
+        public string PrintRN()
+        {
+            return $"{M}/{N}";
+        }
+        public static RationalNum operator *(RationalNum Num1, RationalNum Num2)
+        {
+            int n, m;
+            n = Num1.N * Num2.N;
+            m = Num1.M * Num2.M;
+            return new RationalNum(m, n);
+        }
+        public static RationalNum operator /(RationalNum Num1, RationalNum Num2)
+        {
+            int n, m;
+            n = Num1.N * Num2.M;
+            m = Num1.M * Num2.N;
+            return new RationalNum(m, n);
+        }
+        public static RationalNum operator +(RationalNum Num1, RationalNum Num2)
+        {
+            int n, m;
+            n = Num1.N * Num2.N;
+            m = Num1.M * Num2.N + Num2.M * Num1.N;
+            return new RationalNum(m, n);
+        }
+        public static RationalNum operator -(RationalNum Num1, RationalNum Num2)
+        {
+            int n, m;
+            n = Num1.N * Num2.N;
+            m = Num1.M * Num2.N - Num2.M * Num1.N;
+            return new RationalNum(m, n);
+        }
+        public static bool operator ==(RationalNum Num1, RationalNum Num2)
+        {
+            return Num1.N * Num2.M == Num1.M * Num2.N;
+        }
+        public static bool operator !=(RationalNum Num1, RationalNum Num2)
+        {
+            return Num1.N * Num2.M != Num1.M * Num2.N;
+        }
+        public static bool operator > (RationalNum Num1, RationalNum Num2)
+        {
+            return Num1.N * Num2.M > Num1.M * Num2.N;
+        }
+        public static bool operator < (RationalNum Num1, RationalNum Num2)
+        {
+            return Num1.N * Num2.M < Num1.M * Num2.N;
+        }
+    }
+}
